0b5ee1106e5e90427de42e3ec3f187cbb15dd3e3
Nischal Pandey <162779921+Nischal-pandey@users.noreply.github.com>
2025-04-17 16:26:04 +0530

    pattern practice
    Make for pattern Practice

---
+++ dom.js
@@ -0,0 +1,24 @@
+// methods dom 
+ 
+//  documnet.getelementbyid()
+//  document.getelementsbuclass()
+//  tag name 
+//  .queryselector
+//  .querselectorAll
+
+const n = 5;
+let str = '';
+
+for (let i = 1; i <= n; i++) {
+    let row = '';
+    for (let j = 1; j <= n; j++) {
+        if (j === i || j === n - i + 1) {
+            row += '*';
+        } else {
+            row += ' ';
+        }
+    }
+    str += row + '\n';
+}
+
+console.log(str);
